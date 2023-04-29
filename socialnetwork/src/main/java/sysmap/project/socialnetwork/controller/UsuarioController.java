fdfad3aee65f29918406490227eaade17609179b
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2023-04-29 14:58:39 -0300

    Criando métodos no controller

--- socialnetwork/src/main/java/sysmap/project/socialnetwork/controller/UsuarioController.java
+++ socialnetwork/src/main/java/sysmap/project/socialnetwork/controller/UsuarioController.java
@@ -1,13 +1,26 @@
 package sysmap.project.socialnetwork.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sysmap.project.socialnetwork.model.entities.Usuario;
+import sysmap.project.socialnetwork.repositories.UsuarioRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
     @RequestMapping("/")
     public String getUsuario(){
         return "Testando";
+    }
+    @ModelAttribute("usuarioItems")
+    public List<Usuario> getUsuarios(){
+        return usuarioRepository.findAll();
     }
 }
--- socialnetwork/src/main/java/sysmap/project/socialnetwork/repositories/UsuarioRepository.java
+++ socialnetwork/src/main/java/sysmap/project/socialnetwork/repositories/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package sysmap.project.socialnetwork.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sysmap.project.socialnetwork.model.entities.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 }
